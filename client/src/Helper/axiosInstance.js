d63476ed67519e8bfd64794c696eb8a1f1cc3593
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-05-29 11:23:16 +0530

    Add backend uri

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8081/api/v1",
+    baseURL: "https://lmsazure.azurewebsites.net/api/v1",
     withCredentials: true,
 });
 
